fb03f722e3f12f07f7a6da2920068350f3c8442b
Luis-Esquivia <luisdanielesq7@gmail.com>
2026-01-07 21:42:37 -0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
+/* import icon from './assets/icon.png' */
+/* const icon = require('./assets/icon.png') */
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto"/>
+      <Image 
+        source={{ 
+          uri: "https://elrehilete.wordpress.com/files/2009/03/12.jpg"
+        }}
+        style={{
+          width: 215,
+          height: 294,
+        }}
+      />
+      <Text style={{ color: 'white' }}>Open up App.js to start working on your app!</Text>
+      <Button 
+        title="Pulsa aqui"
+        color="#841584"
+        onPress={() => alert('Hola')}
+      />  
     </View>
   );
 }
@@ -13,7 +30,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
